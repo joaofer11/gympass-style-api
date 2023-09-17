5ee8066a7ecd15345ae816a41cb2b3f259f6fa42
João Fernandes <joaofergear003@gmail.com>
2023-09-17 13:36:41 -0300

    test: add unit test for validate user check-in service
    - it should not be able to validate user check-in upon 20 minutes of its creation;

--- src/services/validate-user-check-in-service.test.ts
+++ src/services/validate-user-check-in-service.test.ts
@@ -1,16 +1,23 @@
-import { it, expect, describe, beforeEach } from 'vitest';
+import { it, vi, expect, describe, beforeEach, afterEach } from 'vitest';
 
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
 import { ValidateUserCheckInService } from './validate-user-check-in-service';
+import { TooLateCheckInValidationError } from './errors/too-late-check-in-validation-error';
 import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-check-in-repository';
 
-describe('Get User Profile Service', () => {
+describe('Validate User Check-in Service', () => {
   let checkInRepository: InMemoryCheckInRepository;
   let sut: ValidateUserCheckInService;
 
   beforeEach(() => {
     checkInRepository = new InMemoryCheckInRepository();
     sut = new ValidateUserCheckInService({ checkInRepository });
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it('should be able to validate user check-in', async () => {
@@ -29,5 +36,22 @@ describe('Get User Profile Service', () => {
     await expect(() =>
       sut.execute({ checkInId: 'NON_EXISTING_ID' })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('it should not be able to validate user check-in upon 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 10, 8, 0, 0));
+
+    const createdCheckIn = await checkInRepository.create({
+      userId: 'user-01',
+      gymId: 'gym-01',
+    });
+
+    const TWENTY_ONE_MINUTES_IN_MS = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(TWENTY_ONE_MINUTES_IN_MS);
+
+    await expect(() =>
+      sut.execute({ checkInId: createdCheckIn.id })
+    ).rejects.toBeInstanceOf(TooLateCheckInValidationError);
   });
 });
